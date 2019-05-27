4e6a7df6cc0a0317ccbd1e8bd282f4675efb96f3
robson rodrigues <robsonrodrigues_12@hotmail.com>
2019-05-26 22:32:15 -0300

    Commit

---
+++ RobsonRodrigues/Controllers/EscolaController.cs
@@ -0,0 +1,23 @@
+﻿using RobsonRodrigues.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RobsonRodrigues.Controllers
+{
+    public class EscolaController : Controller
+    {
+        // GET: Escola
+        public ActionResult Index()
+        {
+            List<AULA_MD> ListarAulas = new List<AULA_MD>();
+            return View(ListarAulas);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ RobsonRodrigues/Robson/AULA.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RobsonRodrigues.Models
+{
+    [MetadataType(typeof(AULA_MD))]
+    public partial class AULA_MD { }
+
+    public class AULA
+    {
+        [DisplayName("Codigo Aula")]
+        public int AulaId { get; set; }
+        [DisplayName("Descricao Aluno")]
+        [Required(ErrorMessage = "Informe a Aula")]
+        [Range(900, 2500, ErrorMessage = "Deve estar")]
+        public string AulaDescricao { get; set; }
+    }
+}
